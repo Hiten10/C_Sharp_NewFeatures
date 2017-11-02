77d17a572ab0ebf3c2e9faad84d149cf4cbc6bfe
vagrant <vagrant@VAGRANT-PC>
2017-11-02 10:36:05 +0530

    Added project files

---
+++ C_Sharp_NewFeatures/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace C_Sharp_NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display("Hitendra", "Kumar");
+            System.Tuple<int, string> info = GetInfo("X1");
+            var info1 = GetInfo1("X");
+            (int age, string name) info3 = GetInfo3("X1");
+            (int age, string name) info4 = GetInfo4("X1");
+            System.Console.WriteLine($"Name: {info.Item2} Age: {info.Item1}");
+            System.Console.WriteLine($"Name: {info1.name} Age: {info1.age}");
+            System.Console.WriteLine($"Name: {info3.name} Age: {info3.age}");
+            System.Console.WriteLine($"Name: {info4.name} Age: {info4.age}");
+            System.Console.Read();
+        }
+
+        public static string Test() => "test";
+        public static void Display(string fname, string lname) => System.Console.WriteLine($"My name is {fname} {lname}");
+
+        public static System.Tuple<int, string> GetInfo(string id) => new System.Tuple<int, string>(36, "Hitendra");
+
+        public static (int age, string name) GetInfo1(string id) => (36, "Hitendra");
+
+        public static (int, string) GetInfo2(string id) => (36, "Hitendra");
+
+        public static (int, string) GetInfo3(string id) => (age: 36, name: "Hitendra");
+
+        public static (int age, string name) GetInfo4(string id) => (age: 36, name: "Hitendra");
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s = new Stack<int>();
+            s.Push(1);
+            s.Push(2);
+            s.Pop();
+            s.Push(1);
+            s.Push(2);
+            s.Pop();
+            s.Pop();
+            s.Pop();
+            s.Push(2);
+            s.Pop();
+            ReadInput();
+            Console.ReadKey();
+
+        }
+
+        static void ReadInput()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            string[] testCases = new string[t];
+            for (int i = 0; i < t; i++)
+            {
+                testCases[i] = Console.ReadLine();
+
+            }
+
+            foreach (string s in testCases)
+            {
+                string[] temp = s.Split(' ');
+                Console.WriteLine(FindLargestNumber(Convert.ToInt64(temp[0]), Convert.ToInt64(temp[1]),
+                              Convert.ToInt64(temp[2])
+                              ));
+            }
+        }
+
+        static long FindLargestNumber(long a, long b, long c)
+        {
+
+            List<int> listRemainders = new List<int>();
+            for (int i = 1; i <= c; i++)
+            {
+                long remainder = i % a;
+                if (remainder == b && remainder <= c)
+                {
+                    listRemainders.Add(i);
+                }
+            }
+
+            return listRemainders.Count() > 0 ? listRemainders.Max() : -1;
+        }
+    }
+}
